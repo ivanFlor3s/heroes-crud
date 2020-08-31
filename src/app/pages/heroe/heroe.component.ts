5d0686817ed1d3bedaa054bf3490899573e07f9b
Ivan <ifloresmanc96@gmail.com>
2020-08-30 23:11:30 -0300

    swal - notifiacion usuario

--- src/app/pages/heroe/heroe.component.ts
+++ src/app/pages/heroe/heroe.component.ts
@@ -3,6 +3,8 @@ import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
 import { HeroeModel } from '../../models/heroe.model';
 import { HeroeServiceService } from '../../services/heroe-service.service';
 
+import  Swal  from 'sweetalert2';
+
 
 @Component({
   selector: 'app-heroe',
@@ -62,16 +64,35 @@ export class HeroeComponent implements OnInit {
     this.heroe.poder = this.heroePoder;
     this.heroe.vivo = this.isAlive;
 
+    Swal.fire({
+      title: 'Espere',
+      text: 'Guardando informacion',
+      icon: 'info',
+      allowOutsideClick: false
+    });
+    Swal.showLoading();
+    
+
     if ( this.heroeId ){
       this.heroeService.actualizarHeroe( this.heroe ).
       subscribe(resp => {
         console.log(resp);
+        Swal.fire({
+          title: this.heroe.nombre,
+          text: 'Se actualizo correctamente',
+          icon: 'success'
+        });
       });
     } else {
       this.heroeService.crearHeroe( this.heroe ).
       subscribe(resp => {
         this.forma.controls['firebaseId'].setValue(resp.id);
         console.log(resp);
+        Swal.fire({
+          title: this.heroe.nombre,
+          text: 'Se actualizo correctamente',
+          icon: 'success'
+        });
       });
     }
 
